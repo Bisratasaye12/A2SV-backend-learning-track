4da1d4c7af04308a69f9d127da5a1bf9d1c56066
Bisratasaye12 <bisrat.asaye@aait.edu.et>
2024-08-09 02:51:48 +0300

    added env and gitignore

--- Task-5/data/init_db.go
+++ Task-5/data/init_db.go
@@ -3,14 +3,26 @@ package data
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // InitDB initializes the MongoDB client and returns it.
 func InitDB() (*mongo.Client, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	if err := godotenv.Load(); err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
+
+    uri := os.Getenv("MONGODB_URI")
+    if uri == "" {
+        log.Fatal("MONGODB_URI environment variable not set")
+    }
+
+	clientOptions := options.Client().ApplyURI(uri)
 
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
